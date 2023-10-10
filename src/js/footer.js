cb37113f517e9b1f1bd4b26029a84c6258846d17
abstractl0gic <zwlyons@gmail.com>
2023-10-10 18:14:59 -0500

    add js templates in src folder

---
+++ src/js/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      {/* Social Links */}
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/js/resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Resume = () => {
+  return (
+    <section id="resume">
+      {/* Resume and Proficiencies */}
+    </section>
+  );
+};
+
+export default Resume;
